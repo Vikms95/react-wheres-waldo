fe2f9a5dc676355131d9c1a593e62e7ab7f0982b
Vikms95 <vms1995@hotmail.com>
2022-06-17 05:29:46 +0200

    Style Leaderboard component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -12,7 +12,7 @@ function Navbar(props: Props) {
       <Link to="/" className="webpage-header">
         <h1> FindMe </h1>
       </Link>
-      <Link to="/leaderboards" className="leaderboards-header">
+      <Link to="/leaderboards" className="webpage-header">
         <h2>Leaderboards</h2>
       </Link>
 
