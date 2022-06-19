9d1d0a7ba9f7ce57b9817deb9f57e12ec9388395
Vikms95 <vms1995@hotmail.com>
2022-06-19 18:34:57 +0200

    Format instructions on Homepage

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -15,25 +15,27 @@ export default function Homepage(props: Props) {
   return (
     <main className="homepage-container">
       <section className="instructions-container">
-        <h3 className="instructions-header">Instructions</h3>
-        <div className="instructions-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Voluptatum nulla ex molestias error cum ipsa, repellendus
-          vitae placeat dolores aliquam pariatur ducimus eum, recusandae
-          consectetur quae. Fugiat tempore asperiores placeat.
-        </div>
-        <article>
-          <h4>How to play</h4>
-          <div>
+        <article className="instructions-article">
+          <h3 className="instructions-header">Instructions</h3>
+          <div className="instructions-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Voluptatum nulla ex molestias error cum ipsa, repellendus
             vitae placeat dolores aliquam pariatur ducimus eum, recusandae
             consectetur quae. Fugiat tempore asperiores placeat.
           </div>
         </article>
-        <article>
-          <h4>Leaderboard</h4>
-          <div>
+        <article className="instructions-article">
+          <h4 className="instructions-header">How to play</h4>
+          <div className="instructions-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatum nulla ex molestias error cum ipsa, repellendus
+            vitae placeat dolores aliquam pariatur ducimus eum, recusandae
+            consectetur quae. Fugiat tempore asperiores placeat.
+          </div>
+        </article>
+        <article className="instructions-article">
+          <h4 className="instructions-header">Leaderboard</h4>
+          <div className="instructions-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Voluptatum nulla ex molestias error cum ipsa, repellendus
             vitae placeat dolores aliquam pariatur ducimus eum, recusandae
