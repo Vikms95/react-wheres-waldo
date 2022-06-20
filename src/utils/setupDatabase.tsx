86e3c4a4ba76f64d40a33c75e8a1e6c840020a10
Vikms95 <vms1995@hotmail.com>
2022-06-20 12:02:08 +0200

    Removed strict mode to avoid duplicating  database creation

--- src/utils/setupDatabase.tsx
+++ src/utils/setupDatabase.tsx
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
-import { addDoc, collection, getFirestore } from 'firebase/firestore';
-import { addSyntheticLeadingComment } from 'typescript';
+import {
+  addDoc, collection, getFirestore, onSnapshot, query, snapshotEqual,
+} from 'firebase/firestore';
 import getConsoleCharacterData from './getConsoleCharactersData';
 
 const firebaseConfig = {
@@ -14,41 +15,45 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
+const characterCoordinates = {
+  'super-nintendo': [
+    { mario: { width: [87, 94], height: [101, 112] } },
+    { chrono: { width: [48, 53], height: [126, 134] } },
+    { zero: [900, 300] },
+  ],
+  'game-cube': [
+    { samus: [500, 200] },
+    { marth: [200, 400] },
+    { toad: [900, 300] },
+  ],
+  'playstation-1': [
+    { mantis: [500, 200] },
+    { vivi: [200, 400] },
+    { alucard: [900, 300] },
+  ],
+  'playstation-2': [
+    { ratchet: [500, 200] },
+    { prince: [200, 400] },
+    { chibi: [900, 300] },
+  ],
+};
+
 /**
  * Sets up character positions data only when the collection does not exist
  */
-const saveCoordinatesToDatabase = async () => {
+const saveCoordinatesToDatabase = () => {
   const consoleNames = ['super-nintendo', 'game-cube', 'playstation-1', 'playstation-2'];
 
   // Only setup the database if no database is created
-  if (collection(getFirestore(), 'coordinates').id.length === 0) {
-    try {
-      await addDoc(collection(getFirestore(), 'coordinates'), {
-        'super-nintendo': [
-          { mario: { width: [87, 94], height: [101, 112] } },
-          { chrono: { width: [48, 53], height: [126, 134] } },
-          { zero: [900, 300] },
-        ],
-        'game-cube': [
-          { samus: [500, 200] },
-          { marth: [200, 400] },
-          { toad: [900, 300] },
-        ],
-        'playstation-1': [
-          { mantis: [500, 200] },
-          { vivi: [200, 400] },
-          { alucard: [900, 300] },
-        ],
-        'playstation-2': [
-          { ratchet: [500, 200] },
-          { prince: [200, 400] },
-          { chibi: [900, 300] },
-        ],
-      });
-    } catch (err) {
-      console.error('Unable to create database: ', err);
+  onSnapshot(query(collection(getFirestore(), 'coordinates')), async (snapshot) => {
+    if (snapshot.docs.length === 0) {
+      try {
+        await addDoc(collection(getFirestore(), 'coordinates'), { characterCoordinates });
+      } catch (err) {
+        console.error('Unable to create database: ', err);
+      }
     }
-  }
+  });
 };
 
 export default saveCoordinatesToDatabase;
